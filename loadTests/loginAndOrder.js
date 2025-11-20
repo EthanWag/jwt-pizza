39e31205e260daa7c3239eb852b404de6e5f0c1d
Ethan Wagstaff <ethanwag2579@gmail.com>
2025-11-19 17:51:25 -0700

    Load testing

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,178 @@
+import { sleep, check, group, fail } from 'k6'
+import http from 'k6/http'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+  },
+}
+
+export function scenario_1() {
+  let response
+
+  // Login
+  response = http.put(
+    'https://pizza-service.ethanwag.click/api/auth',
+    '{"email":"a@jwt.com","password":"admin"}',
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        origin: 'https://pizza.ethanwag.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    }
+  )
+  if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+    console.log(response.body);
+    fail('Login was *not* 200');
+  }
+
+  const authToken = response.json().token;
+
+  sleep(10)
+
+  // Get Menu
+  response = http.get('https://pizza-service.ethanwag.click/api/order/menu', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${authToken}`,
+      'content-type': 'application/json',
+      'if-none-match': 'W/"1fc-cgG/aqJmHhElGCplQPSmgl2Gwk0"',
+      origin: 'https://pizza.ethanwag.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+      'sec-ch-ua-mobile': '?1',
+      'sec-ch-ua-platform': '"Android"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+
+  // Get Franchise
+  response = http.get('https://pizza-service.ethanwag.click/api/franchise?page=0&limit=20&name=*', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${authToken}`,
+      'content-type': 'application/json',
+      'if-none-match': 'W/"5c-UrU6FPurLC0JcnOrzddwdfUXFBA"',
+      origin: 'https://pizza.ethanwag.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+      'sec-ch-ua-mobile': '?1',
+      'sec-ch-ua-platform': '"Android"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(10)
+
+  // Get My Order
+  response = http.get('https://pizza-service.ethanwag.click/api/user/me', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${authToken}`,
+      'content-type': 'application/json',
+      origin: 'https://pizza.ethanwag.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+      'sec-ch-ua-mobile': '?1',
+      'sec-ch-ua-platform': '"Android"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(2.1)
+
+  // Make my Order
+  response = http.post(
+    'https://pizza-service.ethanwag.click/api/order',
+    '{"items":[{"menuId":1,"description":"Veggie","price":0.0038},{"menuId":1,"description":"Veggie","price":0.0038},{"menuId":2,"description":"Pepperoni","price":0.0042}],"storeId":"1","franchiseId":1}',
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${authToken}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.ethanwag.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    }
+  )
+  if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+    console.log(response.body);
+    fail('Order was *not* 200');
+  }
+  sleep(60)
+
+  const pizza = response.json().jwt
+
+  // Verify My PIzza
+  response = http.post(
+    'https://pizza-factory.cs329.click/api/order/verify',
+    `{"jwt":"${pizza}"}`,
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${authToken}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.ethanwag.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-storage-access': 'active',
+      },
+    }
+  )
+
+  if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+    console.log(response.body);
+    fail('Verify was *not* 200');
+  }
+}
